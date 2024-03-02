04fc60ff84ded71cc1bc18513b80615ba6a86821
salman shamsi <shamsisalman81@gmail.com>
2024-03-03 02:59:37 +0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Button from "@mui/material/Button"
 import './App.css'
+import Home from './Screens/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button variant='contained'  >Hello World</Button>
+      <Home/>
     </>
   )
 }
